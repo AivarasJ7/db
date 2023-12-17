fc3d98455035a7e9f96be6e2fc040a550a60016b
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-12-17 13:45:39 +0200

    created footer

--- views/categories/show.php
+++ views/categories/show.php
@@ -41,7 +41,7 @@ $category = CategoriesController::findWithItems($_GET['id']);
                     <div class="col-md-6">
                         <div class="card">
                             <div class="card-body">
-                                <h5 class="card-title"><?= $category->name . " " . $category->description ?></h5>
+                                <h5 class="card-title"><?= $category->description ?></h5>
                             </div>
                             <ul class="list-group list-group-flush">
                                 <?php foreach ($category->items as $item) { ?>
@@ -76,8 +76,33 @@ $category = CategoriesController::findWithItems($_GET['id']);
                     </div>
                 </div>
             </div>
+            <!-- Footer Section -->
+    <footer class="mt-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <h3>Contact Us</h3>
+                    <p>Email: contact@example.com</p>
+                    <p>Phone: +123-456-7890</p>
+                </div>
+                <div class="col-md-4">
+                    <h3>About Us</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                <div class="col-md-4">
+                    <h3>Follow Us</h3>
+                    <p>Connect with us on social media:</p>
+                    <div class="btn-group">
+                        <a href="https://www.facebook.com" class="btn btn-primary me-2" target="_blank">
+                            <i class="fab fa-facebook"></i> Facebook
+                        </a>
+                        <a href="https://twitter.com" class="btn btn-info" target="_blank">
+                            <i class="fab fa-twitter"></i> Twitter
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-</body>
+    </footer>
 
 </html>
